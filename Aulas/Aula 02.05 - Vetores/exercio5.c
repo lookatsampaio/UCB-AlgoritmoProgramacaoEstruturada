2bd83a986ca6d1361a741f400823eb0f3cfe01d6
Lookat Sampaio <lookatsampaio@gmail.com>
2023-05-02 21:48:04 -0300

    duvidas tiradas com a Joyce

--- Aulas/Aula 02.05 - Vetores/exercio5.c
+++ Aulas/Aula 02.05 - Vetores/exercio5.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
-    int vetor[6], i, nBusca, posicao[6], contador = 0;
-    char continua;
+    int vetor[80], i =0, nBusca, posicao[80], contador = 0;
+    char continua[3];
 
     //preencher o vetor de numero inteiros
-    for (i=0; i<6; i++){
+    do{
         printf("Digite o valor: ");
         scanf("%d", &vetor[i]);
-    }
+        i++;
+        printf("Deseja incluir mais um numero?\t Sim ou Nao\n");
+        fflush(stdin);
+        gets(continua);
+    }while ((strcmp(continua, "Sim")==0 || strcmp(continua, "sim")==0) && i<80);
+
+    system("cls");
 
     printf("Digite o numero: ");
     scanf("%d", &nBusca);
 
-    for (i = 0; i < 6; i++){
+    for (i = 0; i < 80; i++){
          if(nBusca == vetor[i]){
             posicao[contador] = i;
             contador++;
          }
     }
 
-    //system("cls");
-
-    
     if(contador == 0){
         printf("Sem numeros semelhantes");
     } else{
