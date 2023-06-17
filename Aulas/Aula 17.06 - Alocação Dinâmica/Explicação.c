c8fb4e8eda174e3a86aa467a7cd2e422eb0595c8
Lookat Sampaio <lookatsampaio@gmail.com>
2023-06-17 11:35:43 -0300

    Final da aula incrivel

--- Aulas/Aula 17.06 - Alocação Dinâmica/Explicação.c
+++ Aulas/Aula 17.06 - Alocação Dinâmica/Explicação.c
@@ -3,21 +3,45 @@
 
 
 
-typedef struct restaurante{
+typedef struct restaurante{ // "typedef" serve para utilizarmos o resumo ou apelido
+    char nome[30], endereco[30], tipoComida[30];
+    double precoMedio;
+    struct restaurante *ponteiro; //definição da variável do tipo ponteiro
 
-char nome[30], endereco[30], tipoComida[30];
-
-double precoMedio;
-
- struct restaurante *ponteiro;
-
-} restaurante;
+} restaurante; //resumo da "struct"
 
 
+restaurante *inicializar (){ //funcao que pode ter qualquer nome
+    return NULL;
+}
+
+
+// tipo de dado = struct        // nome da funcao       //parametros(tipo e o nome da variavel)
+restaurante                     *inserir                (restaurante *cadastroRestaurante){
+    restaurante *novo; //espaco para ponteiro restaurante
+
+    novo = (restaurante*) malloc (sizeof(cadastroRestaurante)); //conversao pq o "malloc" so recebe inteiro
+
+    printf("Digite o nome do restaurante!");
+    scanf("%s", &novo->nome); // nao permite inserir espaco
+    fflush(stdin);
+    printf("Digite o ednereco do restaurante!");
+    fgets (novo->endereco, 30, stdin); //fgets eh so pra string
+    printf("Digite o preco medio!");
+    scanf("%lf", &novo->precoMedio); 
+
+    novo->ponteiro = cadastroRestaurante;
+
+    return novo;
+
+}
 
 
 int main(){
 
-restaurante *cadastroRestaurantes;
+    // restaurante *cadastroRestaurantes[1000]; //alocacao estatica
+    restaurante *cadastroRestaurantes; //alocacao dinamica - lista criada
+    cadastroRestaurantes = inicializar(); //lista vazia igualanda a zero
+    cadastroRestaurantes = inserir(cadastroRestaurantes);
 
 }
